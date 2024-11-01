12de02f73d69fc3baac2d853e4a30ed53c77d5b8
Kutuputih <azkaarrofiq12@gmail.com>
2024-11-01 09:18:54 +0700

    polished

--- Website_Tanaman/script.js
+++ Website_Tanaman/script.js
@@ -2,3 +2,18 @@ document.getElementById("burger").addEventListener("click", function () {
   const nav = document.getElementById("nav");
   nav.classList.toggle("show");
 });
+
+var device_status = document.querySelector(".main_device_status");
+
+function updateDeviceStatus() {
+  if (device_status.textContent === "Connected") {
+    document.querySelector("#color").classList.add("green-dot");
+    document.querySelector("#color").classList.remove("red-dot");
+  } else {
+    document.querySelector("#color").classList.add("red-dot");
+    document.querySelector("#color").classList.remove("green-dot");
+  }
+}
+
+updateDeviceStatus(); // Call the function to execute it
+console.log(device_status.textContent);
